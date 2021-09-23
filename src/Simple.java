98bfea3346e04453d51c67048aae66170b15b0ce
KRATI GOYAL <gkrati04@gmail.com>
2021-09-23 21:39:35 +0530

    check1

--- src/Simple.java
+++ src/Simple.java
@@ -1,17 +1,17 @@
 public class Simple {
-    public int a=10;
+    public static int a=10;
     public static void main(String[] args) {
        Main1 n=new Main1();
        n.test();
       // n.a=10;
        Main2 m=new Main2();
-       m.dis();
+      // m.dis();
     }
 }
 class Main1{
     public void test(){
         //int a=18;
-
+       // int a=18;
         System.out.println(a);
     }
 }
