913d30cd770168e8c550fcf6943be9e35f74470a
KRATI GOYAL <gkrati04@gmail.com>
2021-09-23 20:35:30 +0530

    check

---
+++ src/Simple.java
@@ -0,0 +1,24 @@
+public class Simple {
+    public int a=10;
+    public static void main(String[] args) {
+       Main1 n=new Main1();
+       n.test();
+      // n.a=10;
+       Main2 m=new Main2();
+       m.dis();
+    }
+}
+class Main1{
+    public void test(){
+        //int a=18;
+
+        System.out.println(a);
+    }
+}
+class Main2{
+   // System.out.println(" ");
+    public void dis(int a)
+    {
+        System.out.println(a);
+    }
+}
