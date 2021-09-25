e3af1f72d9a30fd9ce0cb5f3b17a4db41e92d6c1
KRATI GOYAL <gkrati04@gmail.com>
2021-09-25 21:56:20 +0530

    Palindrome

--- src/Stu.java
+++ src/Stu.java
@@ -5,22 +5,30 @@ public class Stu {
 
     public static void main(String[] args) {
         Detail obj=new Detail();
-        obj.display();
+     //   obj.display();
 
 
     }
 }
 class Detail{
-    public int age;
-    public int roll_no;
-    public String name;
- public void display(){
-     Scanner sc=new Scanner(System.in);
-     age=sc.nextInt();
-     roll_no=sc.nextInt();
-     name=sc.next();
-     System.out.println(age);
-     System.out.println(roll_no);
-     System.out.println(name);
- }
+    public int num;
+    public int n;
+    Detail()
+    {
+    Scanner sc=new Scanner(System.in);
+    num=sc.nextInt();
+     //   System.out.println(num);
+        n=num;
+        int re;
+        int sum=0;
+        while(n!=0){
+            re=n%10;
+            sum=sum*10+re;
+            n=n/10;
+        }
+        if(sum==num)
+               System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
 }
