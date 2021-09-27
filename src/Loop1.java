fb395bfae1a294671e2ef80b7c53b183258a95f4
KRATI GOYAL <gkrati04@gmail.com>
2021-09-27 07:33:15 +0530

    Accessibility

---
+++ src/Loop1.java
@@ -0,0 +1,11 @@
+public class Loop1 {
+
+    public static int a=10;
+    public  static int b=10;
+    public static void main(String[] args)
+    {
+        //Loop1 ref=new Loop1();
+        System.out.println(a);
+        System.out.println(Loop1.b);
+    }
+}
