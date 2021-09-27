49c2b3e9d9098d4fee442c5a356ec5f8eb999ba6
KRATI GOYAL <gkrati04@gmail.com>
2021-09-27 06:56:01 +0530

    Multi-Constructor1

--- src/Constructor1.java
+++ src/Constructor1.java
@@ -14,13 +14,15 @@ public class Constructor1 {
         course= sc1.next();
         System.out.println("Course:"+course);
     }
-    Constructor1(int rn)
+    Constructor1(int rn,int pn)
     {
         this();
         rn=15;
+        pn=15;
         System.out.println(rn);
+        System.out.println(pn);
     }
     public static void main(String[] args) {
-        Constructor1 obj=new Constructor1("j");
+        Constructor1 obj=new Constructor1(20,40);
     }
 }
