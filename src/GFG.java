5ba79a800ec56367f116b26b44de7ae5e06a9416
KRATI GOYAL <gkrati04@gmail.com>
2021-09-30 07:49:49 +0530

    Array1

--- src/GFG.java
+++ src/GFG.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 class GFG
 {
     public static void main (String[] args)
     {
-        int[] arr = new int[2];
-        arr[0] = 10;
-        arr[1] = 20;
-
-        for (int i = 0; i <= arr.length; i++)
+        int[] arr = new int[5];
+      //  arr[0] = 10;
+       // arr[1] = 20;
+       for(int i=0;i<arr.length;i++){
+           Scanner sc=new Scanner(System.in);
+           arr[i]=sc.nextInt();
+       }
+        for (int i = 0; i <arr.length; i++)
             System.out.println(arr[i]);
     }
 }
