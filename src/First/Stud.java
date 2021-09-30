c16f66c15328fa9293f08dcfd1bdc3d2f5c19b31
KRATI GOYAL <gkrati04@gmail.com>
2021-09-30 18:20:08 +0530

    Protected Concept

---
+++ src/First/Stud.java
@@ -0,0 +1,14 @@
+package First;
+
+public class Stud {
+     protected int rollno;
+    public String name;
+    public Stud(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+    }
+    public void display(){
+        System.out.println(rollno);
+        System.out.println(name);
+    }
+}
