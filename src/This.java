ab29a7afd3ee1115d30a91de812efff12b0cc55f
KRATI GOYAL <gkrati04@gmail.com>
2021-10-01 23:38:04 +0530

    Getter And Setter

--- src/This.java
+++ src/This.java
@@ -1,3 +1,19 @@
-//public class This {
-//}
-////
+public class This {
+
+    public static void main(String[] args) {
+         OneM obj=new OneM();
+         obj.setName("KRATI GOYAL");
+        System.out.println(obj.getName());
+      //  System.out.println(obj.name);
+    }
+}
+class OneM{
+   private String name;
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newname) {
+        this.name = newname;
+    }
+}
