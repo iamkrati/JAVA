3e01027e08b6cb9f9c9d18b6b6fae589ce5019c6
KRATI GOYAL <gkrati04@gmail.com>
2021-10-01 20:20:49 +0530

    door check using local class

---
+++ src/LocalClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LocalClass {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Door obj=new Door();
+        obj.check(sc.next());
+    }
+}
+class Door{
+    public void check(String key)
+    {
+        class Lock{
+            static String Passkey="YesAccess";
+            public static void result(String tkey){
+                if(Passkey.equals(tkey))
+                  System.out.println("VALID");
+                else
+                   System.out.println("NOT VALID");
+            }
+        }
+        Lock.result(key);
+    }
+}
+
