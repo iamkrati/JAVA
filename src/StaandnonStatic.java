1ea8b3a30cc8687701c9ee702ad5d198595c562d
KRATI GOYAL <gkrati04@gmail.com>
2021-10-05 00:33:57 +0530

    Non Static inner class

---
+++ src/StaandnonStatic.java
@@ -0,0 +1,24 @@
+public class StaandnonStatic {
+
+    public static void main(String[] args) {
+        StaandnonStaati obj=new StaandnonStaati();
+
+    }
+}
+class StaandnonStaati{
+    Inner ob;
+    int n2=20;
+    class Inner{
+        int n2=30;
+        public void display(){
+            int n2=40;
+            System.out.println(n2);
+            System.out.println(this.n2);
+            System.out.println(StaandnonStaati.this.n2);
+        }
+    }
+    StaandnonStaati(){
+        ob=new Inner();
+        ob.display();
+    }
+}
