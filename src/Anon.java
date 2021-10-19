d217aa3d6bda20eacccf00c040302270e515aec4
KRATI GOYAL <gkrati04@gmail.com>
2021-10-19 17:49:48 +0530

    anonymous class

---
+++ src/Anon.java
@@ -0,0 +1,17 @@
+public class Anon {
+    public static void main(String[] args) {
+        // i11 obj=() -> System.out.println("Hello");
+        i111 obj=new i111() {
+            @java.lang.Override
+            public void m1() {
+                System.out.println("hello");
+            }
+        };
+        obj.m1();
+    }
+}
+@FunctionalInterface
+interface i111 {
+    public void m1();
+    //public void m2();
+}
