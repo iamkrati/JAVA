215b8262e1339c702d5461a3e974a688e07ed35d
KRATI GOYAL <gkrati04@gmail.com>
2021-11-09 23:03:30 +0530

    Default values output based question

---
+++ src/Stri1.java
@@ -0,0 +1,20 @@
+//class MyString{
+//    String str;
+//    MyString(String str)
+//    {
+//        this.str=str;
+//    }
+//}
+public class Stri1 {
+    boolean b;
+    int I;
+    int ia[]=new int[1];
+    Object o;
+    public static void main(String[] args) {
+       Stri1 obj=new Stri1();
+       obj.print();
+    }
+    public void print(){
+        System.out.println(ia[0]+" " +b+" " +I+" "+o);
+    }
+}
