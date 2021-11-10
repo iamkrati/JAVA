bab984c87a05916bfb3b48c7485ee7eb5e3c472d
KRATI GOYAL <gkrati04@gmail.com>
2021-11-10 17:42:39 +0530

    string related

---
+++ src/Block.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+
+public class Block {
+//    Object o;
+//    public void print(){
+//        System.out.println(o);
+//    }
+    public static void main(String[] args) {
+        //System.out.println(o);
+     String john="john";
+     String jon=new String(john);
+        System.out.println((john==jon)+" "+(jon.equals(john)));
+        }
+    }
+//class MyString{
+//    String str;
+//    MyString(String str){
+//        this.str=str;
+//    }
+//}
+
