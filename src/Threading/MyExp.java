84d692a4d8f6deef0641573d56a50714104da869
KRATI GOYAL <gkrati04@gmail.com>
2021-11-16 22:26:56 +0530

    Multithreading

---
+++ src/Threading/MyExp.java
@@ -0,0 +1,17 @@
+package Threading;
+
+public class MyExp {
+    public static void main(String[] args) {
+        Thread obj = Thread.currentThread();
+        System.out.println(obj.getName());
+        obj.setName("Krati");
+        System.out.println(obj.getName());
+        System.out.println(obj.getId());
+        System.out.println(obj.isAlive());
+        System.out.println(obj.getPriority());
+        obj.setPriority(Thread.MIN_PRIORITY);
+        System.out.println(obj.getPriority());
+        System.out.println(obj.isDaemon());
+
+    }
+}
---
+++ src/Threading/Ways.java
@@ -0,0 +1,12 @@
+package Threading;
+
+public class Ways {
+    /* two ways to create multithreaded application
+    1.by extending the Thread class
+    2.by implementing the Runnable interface
+    it is known as functional interface
+    run() method
+    lambda expression
+     */
+
+}
