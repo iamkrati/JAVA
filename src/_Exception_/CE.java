27dccd477b56d9f84320f740ba1b16391afecd3e
KRATI GOYAL <gkrati04@gmail.com>
2021-11-17 00:24:35 +0530

    1CE

--- src/_Exception_/CE.java
+++ src/_Exception_/CE.java
@@ -1,7 +1,7 @@
 package _Exception_;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,7 +9,7 @@ public class CE {
     public static void main(String[] args) throws IOException {
         FileReader filee = new FileReader("C:\\test\\a.txt");
         BufferedReader fileInput= new BufferedReader(filee);
-        for (int counter = 0; counter < 3; counter++)
+        for (int i=0;i<3;i++)
             System.out.println(fileInput.readLine());
         fileInput.close();
     }
