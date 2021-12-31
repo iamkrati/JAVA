15487cca9bb9d01d429bb177d49804c13ca559dc
KRATI GOYAL <gkrati04@gmail.com>
2021-12-31 14:15:37 +0530

    arraylist

--- src/Collection_Framework/Demo.java
+++ src/Collection_Framework/Demo.java
@@ -1,4 +1,13 @@
 package Collection_Framework;
 
+import java.util.ArrayList;
+
 public class Demo {
+    public static void main(String[] args) {
+        ArrayList<String> obj=new ArrayList<>();
+        obj.add("krati");
+        obj.add("ayushi");
+        obj.add("MK");
+        System.out.println(obj);
+    }
 }
