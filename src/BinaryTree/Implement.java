8272bfced809c1df4efa79adec550bbd468ae4e8
KRATI GOYAL <gkrati04@gmail.com>
2022-03-31 11:10:49 +0530

    BSTtree

---
+++ src/BinaryTree/Implement.java
@@ -0,0 +1,52 @@
+package BinaryTree;
+
+public class Implement {
+    Node root;
+   class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val=val;
+    }
+   }
+   Implement(){root=null;}
+
+    public static void main(String[] args) {
+       Implement obj=new Implement();
+       obj.root=obj.insert(obj.root,14);
+       obj.root= obj.insert(obj.root,18);
+       obj.root=obj.insert(obj.root,12);
+       obj.root=obj.insert(obj.root,15);
+        obj.root=obj.insert(obj.root,19);
+        obj.root=obj.insert(obj.root,10);
+        obj.root=obj.insert(obj.root,13);
+       obj.inorder(obj.root);
+    }
+    public Node insert(Node root,int val){
+        //Node root=null;
+        //root=insert();
+        Node nnode=new Node(val);
+        if(root==null){
+            root=nnode;
+            return root;
+        }
+        if(val<root.val){
+            root.left=insert(root.left,val);
+        }
+        else if(val>root.val){
+            root.right=insert(root.right,val);
+        }
+        return root;
+        }
+        public void inorder(Node root){
+            if(root==null){
+//                System.out.println(root.val);
+                return;
+            }
+            inorder(root.left);
+            System.out.println(root.val);
+            inorder(root.right);
+        }
+    }
+
